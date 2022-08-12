15741b626d7c9f6a4ab35dd4aef13b0e1de44f3b
Nguyễn Văn Hằng <hangnguyen19091991@gmail.com>
2022-08-12 17:45:19 +0700

    them topic

---
+++ components/TopicForm.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="mt-3">
+        <div class="d-flex">
+            <TitleHeader :title="topic ? titleEdit : titleAdd" />
+            <BaseButton class="btn-primary ms-auto" :btnType="'submit'" 
+                :name="'Save'" :textSize="'text-small'" />
+        </div>
+
+        <div class="row mt-3">
+            
+        </div>
+        
+    </div>
+</template>
+<script>
+import { ref } from 'vue';
+import TitleHeader from './common/TitleHeader.vue';
+import BaseButton from './common/BaseButton.vue';
+export default {
+    components: { TitleHeader, BaseButton },
+    props: ['topic'],
+    data() {
+        return {
+            titleAdd: 'Giao diện thêm mới chủ đề',
+            titleEdit: 'Giao diện chỉnh sửa chủ đề'
+        }
+    },
+    setup(props) {
+        const topic = ref(props.news && null);
+
+        return {
+            topic
+        }
+    }
+}
+</script>
+<style lang="scss">
+    
+</style>
---
+++ pages/topic/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="mt-3">
+        <div class="d-flex mb-3">
+            <TitleHeader :title="titleForm" />
+            
+        </div>
+
+        <div class="table-content box p-3">
+            <TableComponent :headers="tableHeader" :items="tableData"
+                :actionEdit="true"
+            />
+        </div>
+
+        
+    </div>
+</template>
+<script>
+import { ref } from 'vue';
+import TitleHeader from '~~/components/common/TitleHeader.vue';
+import AddButton from '~~/components/common/AddButton.vue';
+import TableComponent from '~~/components/common/table/TableNewsComponent.vue';
+export default {
+    components: { TitleHeader, AddButton, TableComponent },
+    data() {
+        return {
+            titleForm: "Danh sách chủ đề",
+            titleButtonAdd: "Thêm mới",
+            routerPush: "/topic/form",
+        }
+    },
+    setup() {
+        const tableHeader = [
+            { text: "No", value: "no" },
+            { text: "Name", value: "name" }
+        ];
+
+        const tableData = ref([]);
+
+        return {
+            tableHeader,
+            tableData
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
