9536b7a3d92a1107887cea691768fd1671c59b43
Nguyễn Văn Hằng <hangnguyen19091991@gmail.com>
2022-08-17 09:21:54 +0700

    Sửa date lỗi tháng và form search

--- components/common/table/TableNewsComponent.vue
+++ components/common/table/TableNewsComponent.vue
@@ -1,36 +1,58 @@
 <template>
-    <table class="table table-striped table-hover">
-        <thead>
-            <tr>
-                <th v-for="(item, index) in headers" :key="index">
-                    {{ item.text }}
-                </th>
-                <th class="text-center" v-if="actionEdit || actionDelete">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="(item, index) in items" :key="index">
-                <td class="text-center">{{ (index + 1) + page*size }}</td>
-                <td>{{ item.title }}</td>
-                <td>{{ displayBrief(item.brief) }}</td>
-                <td>{{ displayDate(item.createdDate) }}</td>
-                <td>
-                    <template v-if="item.status == 'new'"><span class="badge bg-primary">Thêm mới</span></template>
-                    <template v-if="item.status == 'approved'"><span class="text-success">Đã phê duyệt</span></template>
-                </td>
-                <td class="" v-if="actionEdit || actionDelete">
-                    <div class="d-flex">
-                        <div class="ms-auto cursor-pointer" v-if="actionEdit">
-                            <NuxtLink :to="'/news/form/' + item.id" class="d-flex"><edit-icon /><span class="ms-1">Edit</span></NuxtLink>
+    <div>
+        <div class="d-flex mb-2">
+            <div class="search-field d-flex">
+                <span class="search-title d-block m-auto">Field</span>
+                <select class="form-select ms-2 d-block minW-100">
+                    <option>a</option>
+                </select>
+            </div>
+            
+            <div class="search-type ms-2 d-flex">
+                <span class="search-title d-block m-auto">Type</span>
+                <select class="form-select ms-2 d-block minW-100">
+                    <option value="">>=</option>
+                </select>
+            </div>
+
+            <div class="search-value ms-2 d-flex">
+                <span class="search-value d-block m-auto">Value</span>
+                <input type="input" class="form-control ms-2" placeholder="Search..." />
+            </div>
+        </div>
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th v-for="(item, index) in headers" :key="index">
+                        {{ item.text }}
+                    </th>
+                    <th class="text-center" v-if="actionEdit || actionDelete">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item, index) in items" :key="index">
+                    <td class="text-center">{{ (index + 1) + page*size }}</td>
+                    <td>{{ item.title }}</td>
+                    <td>{{ displayBrief(item.brief) }}</td>
+                    <td>{{ displayDate(item.createdDate) }}</td>
+                    <td>
+                        <template v-if="item.status == 'new'"><span class="badge bg-primary">Thêm mới</span></template>
+                        <template v-if="item.status == 'approved'"><span class="text-success">Đã phê duyệt</span></template>
+                    </td>
+                    <td class="" v-if="actionEdit || actionDelete">
+                        <div class="d-flex">
+                            <div class="ms-auto cursor-pointer" v-if="actionEdit">
+                                <NuxtLink :to="'/news/form/' + item.id" class="d-flex"><edit-icon /><span class="ms-1">Edit</span></NuxtLink>
+                            </div>
+                            <div class="d-flex me-auto cursor-pointer ms-3 text-danger" v-if="actionDelete">
+                                <delete-icon @click="disabledNews(item.id)" /> <span class="ms-1">Hide</span>
+                            </div>
                         </div>
-                        <div class="d-flex me-auto cursor-pointer ms-3 text-danger" v-if="actionDelete">
-                            <delete-icon @click="disabledNews(item.id)" /> <span class="ms-1">Hide</span>
-                        </div>
-                    </div>
-                </td>
-            </tr>
-        </tbody>
-    </table>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 <script>
 import moment from "moment";
@@ -60,7 +82,7 @@ export default {
         }
 
         function displayDate(date) {
-            return moment(date).format('YYYY-MM-DD HH:mm:ss');
+            return moment(date).month(date[1] - 1).format('YYYY-MM-DD HH:mm:ss');
         }
 
         return {
