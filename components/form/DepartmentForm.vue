be7e81ababbd472eb5185045623cd7637cbc28e7
tuannghia11 <avicii1129@gmail.com>
2022-09-26 09:35:55 +0700

    update deleting comment

--- components/form/DepartmentForm.vue
+++ components/form/DepartmentForm.vue
@@ -61,7 +61,6 @@ export default {
       name: ""
     });
     const header = useHeader();
-    const { $showToast } = useNuxtApp();
     function validateName(value) {
       if (!value)
         return "Trường này là bắt buộc";
